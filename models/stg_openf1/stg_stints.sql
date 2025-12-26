654be41b1ab047f6c48114646af9dfb71209ea67
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-26 15:14:23 +0000

    stg_pits and stg_stints are added in staging layer

---
+++ models/stg_openf1/stg_stints.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+select
+  session_key::number    as session_key,
+  driver_number::number  as driver_number,
+  stint_number::number   as stint_number,
+  compound::string       as compound,
+  lap_start::number      as lap_start,
+  lap_end::number        as lap_end
+from {{ source('raw_openf1','OPENF1_STINTS') }}

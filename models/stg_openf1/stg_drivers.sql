036701b9f7d8dead66af6986ecbe186e330ba81e
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-26 14:54:32 +0000

    staging drivers and sessions

---
+++ models/stg_openf1/stg_drivers.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+SELECT
+    session_key::number         as session_key,
+    driver_number::number       as driver_number,
+    full_name::string           as full_name,
+    name_acronym::string        as name_acronym,
+    team_name::string           as team_name,
+    team_color::string          as team_color,
+    broadcast_name::string      as broadcast_name,
+    country_code::string        as country_code
+FROM {{source('raw_openf1','OPENF1_DRIVERS') }}
---
+++ models/stg_openf1/stg_sessions.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='view') }}
+
+SELECT
+    session_key::number             as session_key,
+    meeting_key::number             as meeting_key,
+    session_name::string            as session_name,
+    session_type::string            as session_type,
+    country_name::string            as country_name,
+    location::string                as location,
+    circuit_short_name:: string     as circuit_short_name,
+    to_timestamp_tz(date_start)     as date_start_utc,
+    to_timestamp_tz(date_end)       as date_end_utc,
+    gmt_offset::string              as gmt_offset,
+    year::number                    as year 
+
+FROM {{ source('raw_openf1','OPENF1_SESSIONS') }}

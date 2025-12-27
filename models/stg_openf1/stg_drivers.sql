f79d5abd04eb340945f0dc45be17e54fdc12fdb0
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-27 20:55:21 +0000

    removed country code

--- models/stg_openf1/stg_drivers.sql
+++ models/stg_openf1/stg_drivers.sql
@@ -8,5 +8,5 @@ SELECT
     team_name::string           as team_name,
     team_colour::string          as team_color,
     broadcast_name::string      as broadcast_name,
-    country_code::string        as country_code
+
 FROM {{source('raw_openf1','OPENF_1_DRIVERS') }}

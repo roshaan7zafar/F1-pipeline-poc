593edee54306d5e9a4b227e80a2c6cf4183e3ad6
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-27 20:52:18 +0000

    stg_drivers.sql team colours

--- models/stg_openf1/stg_drivers.sql
+++ models/stg_openf1/stg_drivers.sql
@@ -6,7 +6,7 @@ SELECT
     full_name::string           as full_name,
     name_acronym::string        as name_acronym,
     team_name::string           as team_name,
-    team_color::string          as team_color,
+    team_colour::string          as team_color,
     broadcast_name::string      as broadcast_name,
     country_code::string        as country_code
 FROM {{source('raw_openf1','OPENF_1_DRIVERS') }}

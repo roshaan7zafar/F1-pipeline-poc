25cfd4a6244b43f42a3c9db98068a0d0c4953e95
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-27 21:50:12 +0000

    dim_driver team color

--- models/marts_openf1/dim_driver.sql
+++ models/marts_openf1/dim_driver.sql
@@ -3,5 +3,5 @@
 select
   {{ dbt_utils.generate_surrogate_key(['session_key','driver_number']) }} as driver_sk,
   session_key, driver_number,
-  full_name, name_acronym, team_name, team_colour
+  full_name, name_acronym, team_name, team_color
 from {{ ref('stg_drivers') }}

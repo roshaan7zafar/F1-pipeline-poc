97526755faead8481133877b69ecbf134b6ba59c
mohammad roshaan butt <roshaanzafar619@gmail.com>
2025-12-27 20:39:51 +0000

    added _int_schema.yml and intermediate layer models

--- models/int_openf1/int_laps_enriched.sql
+++ models/int_openf1/int_laps_enriched.sql
@@ -1,1 +1,22 @@
 {{ config(materialized='view') }}
+
+with laps as (
+  select * from {{ ref('stg_laps') }}
+),
+drivers as (
+  select session_key, driver_number, full_name, name_acronym, team_name, team_colour
+  from {{ ref('stg_drivers') }}
+)
+select
+  l.session_key,
+  l.driver_number,
+  l.lap_number,
+  l.lap_time_s,
+  l.is_pit_out_lap,
+  -- "clean lap" definition: timed and not pit-out (tweak as you like)
+  case when l.lap_time_s is not null and coalesce(l.is_pit_out_lap,false)=false
+       then true else false end as is_clean_lap,
+  d.full_name, d.name_acronym, d.team_name, d.team_colour
+from laps l
+left join drivers d
+  on d.session_key = l.session_key and d.driver_number = l.driver_number
